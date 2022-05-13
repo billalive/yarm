3ffd7ddbf160d6c04b4730bc0063bb9f552a6ed1
Bill Alive <public+git@billalive.com>
2022-05-13 12:56:56 -0400

    test: minimum working test with pytest

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import click.testing
+import pytest
+
+from yarm import console
+
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeeds(runner):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
