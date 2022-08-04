b5f9bddf90d85cc9abd2d54aec7563e99e165c56
Bill Alive <public+git@billalive.com>
2022-08-04 14:06:02 -0400

    test: new succeeds

--- Makefile
+++ Makefile
@@ -27,3 +27,7 @@ list-sessions:
 # https://cookiecutter-hypermodern-python.readthedocs.io/en/2022.6.3.post1/guide.html#version-constraints
 lock:
 	poetry lock --no-update
+
+coverage:
+	coverage html
+	xdg-open htmlcov/index.html
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,13 @@
 """Test cases for the __main__ module."""
+import importlib.resources as pkg_resources
+
 import pytest
 from click.testing import CliRunner
 
 from yarm import __main__
+from yarm.__main__ import cli
+from yarm.__main__ import default_config_file
+from yarm.__main__ import dir_templates
 
 
 @pytest.fixture
@@ -14,4 +19,16 @@ def runner() -> CliRunner:
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.cli)
+    assert result.exit_code == 0
+
+
+def test_default_config_template_exists() -> None:
+    """The default config file template exists."""
+    # assert pkg_resources.is_resource("yarm", template_config_name)
+    assert pkg_resources.is_resource(f"yarm.{dir_templates}", default_config_file)
+
+
+def test_new_succeeds(runner: CliRunner) -> None:
+    """It exits with a status code of zero."""
+    result = runner.invoke(cli, ["new"])
     assert result.exit_code == 0
